656b4b5e64880076bde4eb183e4e1005b825b6ee
Justin Brierley <91489058+paradingwolves@users.noreply.github.com>
2023-09-12 15:35:25 -0400

    Update AddStock.jsx

--- src/components/AddStock/AddStock.jsx
+++ src/components/AddStock/AddStock.jsx
@@ -1,47 +1,15 @@
-import React, { useEffect, useRef } from 'react';
-import Header from '../Layout/Header';
-import Footer from '../Layout/Footer';
+import React from 'react'
+import Header from '../Layout/Header'
+import Footer from '../Layout/Footer'
 
 const AddStock = () => {
-  const videoRef = useRef(null);
-
-  useEffect(() => {
-    // Function to start the camera stream
-    const startCamera = async () => {
-      try {
-        const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-        if (videoRef.current) {
-          videoRef.current.srcObject = stream;
-        }
-      } catch (error) {
-        console.error('Error accessing camera:', error);
-      }
-    };
-
-    startCamera(); // Start the camera when the component mounts
-
-    // Function to stop the camera stream when the component unmounts
-    return () => {
-      if (videoRef.current) {
-        const stream = videoRef.current.srcObject;
-        if (stream) {
-          const tracks = stream.getTracks();
-          tracks.forEach((track) => track.stop());
-        }
-      }
-    };
-  }, []);
-
   return (
     <div>
         <Header />
-      <h1>Camera Access Example</h1>
-      <div>
-        <video ref={videoRef} autoPlay playsInline muted />
-      </div>
-      <Footer />
-    </div>
-  );
-};
 
-export default AddStock;
+        <Footer />
+    </div>
+  )
+}
+
+export default AddStock
