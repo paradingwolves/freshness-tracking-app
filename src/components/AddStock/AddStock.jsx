1554c2b366567a1f9d575fa584001525498bfca5
Justin Brierley <91489058+paradingwolves@users.noreply.github.com>
2023-09-15 15:25:44 -0400

    test scanner

--- src/components/AddStock/AddStock.jsx
+++ src/components/AddStock/AddStock.jsx
@@ -1,56 +1,39 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Header from '../Layout/Header';
 import Footer from '../Layout/Footer';
+import {QrReader} from 'react-qr-reader';
 
 const AddStock = () => {
   const videoRef = useRef(null);
+  const [result, setResult] = useState(null);
 
-  useEffect(() => {
-    // Function to start the rear camera stream
-    const startRearCamera = async () => {
-      try {
-        const stream = await navigator.mediaDevices.getUserMedia({
-          video: { facingMode: 'environment' }, // Request rear camera
-        });
-        if (videoRef.current) {
-          videoRef.current.srcObject = stream;
-        }
-      } catch (error) {
-        console.error('Error accessing rear camera:', error);
-      }
-    };
+  const handleScan = (data) => {
+    if (data) {
+      setResult(data);
+    }
+  };
 
-    startRearCamera(); // Start the rear camera when the component mounts
-
-    // Create a variable to hold the ref value
-    const currentVideoRef = videoRef.current;
-
-    // Function to stop the camera stream when the component unmounts
-    return () => {
-      if (currentVideoRef) {
-        const stream = currentVideoRef.srcObject;
-        if (stream) {
-          const tracks = stream.getTracks();
-          tracks.forEach((track) => track.stop());
-        }
-      }
-    };
-  }, []);
+  const handleError = (error) => {
+    console.error('Error accessing camera:', error);
+  };
 
   return (
     <div>
       <Header />
       <div className="container bg-info">
-        <h1 className='text-dark fw-bold text-center'>Rear Camera Access Example</h1>
+        <h1 className="text-dark fw-bold text-center">Rear Camera Access Example</h1>
         <div>
           <h5 className="fw-bold text-center text-white">Scan Bar Code</h5>
-          <video
-            ref={videoRef}
-            autoPlay
-            playsInline
-            muted
-            style={{ maxWidth: '100%' }} // Set the maximum width to 80%
+          <QrReader
+            delay={300}
+            onError={handleError}
+            onScan={handleScan}
+            style={{ width: '100%' }}
           />
+        </div>
+        <div>
+          <h3>Result:</h3>
+          {result && <p>{result}</p>}
         </div>
       </div>
       <Footer />
