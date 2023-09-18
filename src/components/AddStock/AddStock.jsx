506841741647bf5e28a6704097fab9f5932e3a75
Justin Brierley <91489058+paradingwolves@users.noreply.github.com>
2023-09-18 11:00:59 -0400

    Test Scanner Again

--- src/components/AddStock/AddStock.jsx
+++ src/components/AddStock/AddStock.jsx
@@ -11,13 +11,18 @@ const AddStock = () => {
   const [detectedBarcode, setDetectedBarcode] = useState('');
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const [matchingItems, setMatchingItems] = useState([]); // State to hold matching items
+  const [scanningEnabled, setScanningEnabled] = useState(true); // Control scanner state
 
   const openModal = () => {
     setModalIsOpen(true);
+    // Disable scanning when the modal is open
+    setScanningEnabled(false);
   };
 
   const closeModal = () => {
     setModalIsOpen(false);
+    // Re-enable scanning when the modal is closed
+    setScanningEnabled(true);
     // Clear matching items when modal is closed
     setMatchingItems([]);
   };
@@ -42,51 +47,53 @@ const AddStock = () => {
   }, []);
 
   useEffect(() => {
-    Quagga.init(
-      {
-        inputStream: {
-          type: 'LiveStream',
-          constraints: {
-            width: { min: 640 },
-            height: { min: 480 },
+    if (scanningEnabled) {
+      Quagga.init(
+        {
+          inputStream: {
+            type: 'LiveStream',
+            constraints: {
+              width: { min: 640 },
+              height: { min: 480 },
+            },
+            target: videoRef.current,
           },
-          target: videoRef.current,
+          decoder: {
+            readers: ['code_128_reader', 'ean_reader', 'upc_reader', 'code_39_reader'],
+          },
         },
-        decoder: {
-          readers: ['code_128_reader', 'ean_reader', 'upc_reader', 'code_39_reader'],
-        },
-      },
-      (err) => {
-        if (err) {
-          console.error('QuaggaJS initialization error:', err);
-          return;
+        (err) => {
+          if (err) {
+            console.error('QuaggaJS initialization error:', err);
+            return;
+          }
+          Quagga.start();
         }
-        Quagga.start();
-      }
-    );
+      );
 
-    Quagga.onDetected(async (result) => {
-      const barcodeValue = result.codeResult.code;
-      const sanitizedBarcode = barcodeValue.startsWith('0') ? barcodeValue.substring(1) : barcodeValue;
-      console.log('Detected barcode:', sanitizedBarcode);
-      setDetectedBarcode(sanitizedBarcode);
+      Quagga.onDetected(async (result) => {
+        const barcodeValue = result.codeResult.code;
+        const sanitizedBarcode = barcodeValue.startsWith('0') ? barcodeValue.substring(1) : barcodeValue;
+        console.log('Detected barcode:', sanitizedBarcode);
+        setDetectedBarcode(sanitizedBarcode);
 
-      // Open the modal when a barcode is detected
-      openModal();
+        // Open the modal when a barcode is detected
+        openModal();
 
-      // Fetch matching items from Firestore based on barcode_number
-      const stockRef = collection(db, 'Stock');
-      const q = query(stockRef, where('barcode_number', '==', sanitizedBarcode));
-      const querySnapshot = await getDocs(q);
+        // Fetch matching items from Firestore based on barcode_number
+        const stockRef = collection(db, 'Stock');
+        const q = query(stockRef, where('barcode_number', '==', sanitizedBarcode));
+        const querySnapshot = await getDocs(q);
 
-      const matchingItemsData = querySnapshot.docs.map((doc) => doc.data());
-      setMatchingItems(matchingItemsData);
-    });
+        const matchingItemsData = querySnapshot.docs.map((doc) => doc.data());
+        setMatchingItems(matchingItemsData);
+      });
 
-    return () => {
-      Quagga.stop();
-    };
-  }, []);
+      return () => {
+        Quagga.stop();
+      };
+    }
+  }, [scanningEnabled]);
 
   return (
     <div>
@@ -118,15 +125,15 @@ const AddStock = () => {
       >
         <h2>Detected Barcode</h2>
         <p>Item Number: {detectedBarcode}</p>
-        
+
         {/* Display matching items */}
         <h3>Matching Items:</h3>
         <ul>
           {matchingItems.map((item) => (
-            <li key={item.id}>{item.name}</li>
+            <li key={item.id}>{item.item_name}</li>
           ))}
         </ul>
-        
+
         <button onClick={closeModal}>Close</button>
       </Modal>
     </div>
