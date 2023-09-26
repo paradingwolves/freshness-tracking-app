0a6f0085db1aa594491a6f053b75f4919a26b7ad
Justin Brierley <91489058+paradingwolves@users.noreply.github.com>
2023-09-26 12:55:11 -0400

    try again...

--- src/components/AddStock/AddStock.jsx
+++ src/components/AddStock/AddStock.jsx
@@ -6,7 +6,8 @@ import Modal from 'react-modal';
 import useMatchingStockData from '../../hooks/ScanStock'; // Import the hook
 import { db } from '../../lib/firebase'; // Import Firebase configuration
 import { collection, getDocs } from 'firebase/firestore';
-import useAddStock from '../../hooks/AddStock'; // Import the useAddStock hook
+import useAddStock from '../../hooks/AddStock';
+
 
 const AddStock = () => {
   const videoRef = useRef(null);
@@ -15,18 +16,13 @@ const AddStock = () => {
   const [scanningEnabled, setScanningEnabled] = useState(true); // Control scanner state
   const [stockData, setStockData] = useState([]); // State to hold Stock data
   const { matchingItems, startScanning, stopScanning } = useMatchingStockData(detectedBarcode); // Use the hook
-  const { addStockItem, isLoading } = useAddStock(); // Use the useAddStock hook
-  const [formData, setFormData] = useState({
-    name: '',
-    brand: '',
-    quantity: '',
-    expiry_date: '',
-    item_number: '',
-    barcode_number_number: '',
-    updated: '',
-    animal: '',
-    // Add more fields as needed
-  });
+
+  const {
+    selectedItem,
+    selectItem,
+    clearSelectedItem,
+    updateStockQuantity,
+  } = useAddStock();
 
   const openModal = () => {
     setModalIsOpen(true);
@@ -111,50 +107,16 @@ const AddStock = () => {
 
     fetchStockData();
   }, []);
-
-  const handleSubmit = async () => {
-    // Use matching stock data as the initial form data
-    const matchingItem = matchingItems[0]; // Assuming there's only one matching item
-    const formDataToUpdate = { ...formData };
-    formDataToUpdate.name = matchingItem.name;
-    formDataToUpdate.brand = matchingItem.brand;
-    formDataToUpdate.quantity = matchingItem.quantity;
-    formDataToUpdate.item_number = matchingItem.item_number;
-    formDataToUpdate.barcode_number = matchingItem.barcode_number;
-    formDataToUpdate.animal = matchingItem.animal;
-    formDataToUpdate.updated = matchingItem.updated;
-  
-    // Convert the expiry_date to a Unix timestamp for midnight of that day
-    // Convert the expiry_date to a Unix timestamp for midnight of that day
-  if (formData.expiry_date) {
-    const [year, month, day] = formData.expiry_date.split('-').map(Number);
-    if (!isNaN(year) && !isNaN(month) && !isNaN(day)) {
-      const expiryDate = new Date(year, month - 1, day); // Months are 0-based
-      expiryDate.setHours(0, 0, 0, 0); // Set time to midnight
-      formDataToUpdate.expiry_date = expiryDate.getTime() / 1000;
-    } else {
-      console.error('Invalid expiry date format');
-      return; // Prevent further execution if the date is invalid
-    }
-  }
-
-  
-    // Call the addStockItem function to add the item to Firebase
-    const addedSuccessfully = await addStockItem(detectedBarcode, formDataToUpdate);
-  
-    if (addedSuccessfully) {
-      // Optionally, you can show a success message or clear the form here
-      console.log('Item added successfully');
-    } else {
-      // Handle the error or display an error message
-      console.error('Failed to add item');
-    }
+  const handleQuantityChange = (event) => {
+    const newQuantity = event.target.value;
+    updateStockQuantity(newQuantity);
   };
+
   return (
     <div>
       <Header />
       <div className="container bg-light p-4 rounded">
-        <h1 className='text-dark fw-bold text-center'>Add Inventory</h1>
+        <h1 className="text-dark fw-bold text-center">Add Inventory</h1>
         <div className="text-center my-3">
           <h5 className="fw-bold text-dark">Scan Barcodes</h5>
           <video
@@ -173,90 +135,52 @@ const AddStock = () => {
         onRequestClose={closeModal}
         contentLabel="Detected Barcode Modal"
       >
-        {/* Display matching items or Stock data */}
-        <h3>Matching Items:</h3>
-        <form>
-          {matchingItems.map((item, index) => (
-            <div key={index} className="mb-3">
+        {selectedItem ? (
+          <div>
+            <h3>Selected Item:</h3>
+            <div className="mb-3">
               <label className="form-label">Name</label>
               <input
                 type="text"
                 className="form-control"
-                value={item.name}
+                value={selectedItem.name}
                 required
                 disabled
               />
-              <label className="form-label">Brand</label>
-              <input
-                type="text"
-                className="form-control"
-                value={item.brand}
-                required
-                disabled
-              />
+              {/* ... Include other input fields for the selected item */}
               <label className="form-label">Quantity</label>
               <input
-                type="number"
-                className="form-control"
-                required
-                value={item.quantity}
-                onChange={(e) => setFormData({ ...formData, quantity: e.target.value })}
-
-              />
-              <label className="form-label">Expiry Date</label>
-              <input
-                type="date"
-                className="form-control"
-                value={formData.expiry_date}
-                onChange={(e) => setFormData({ ...formData, expiry_date: e.target.value })}
-                required
-              />
-              <label className="form-label">Updated</label>
-              <input
-                type="number"
-                className="form-control"
-                value={item.updated}
-                onChange={(e) => setFormData({ ...formData, updated: e.target.value })}
-                required
-              />
-              <label className="form-label">Item Number</label>
-              <input
                 type="text"
                 className="form-control"
-                value={item.item_number}
                 required
-                disabled
+                value={selectedItem.quantity}
+                onChange={handleQuantityChange}
               />
-              <label className="form-label">Barcode Number</label>
-              <input
-                type="text"
-                className="form-control"
-                value={item.barcode_number}
-                required
-                disabled
-              />
-              <label className="form-label">Animal</label>
-              <input
-                type="text"
-                className="form-control"
-                value={item.animal}
-                required
-                disabled
-              />
-              {/* Add other fields as needed */}
             </div>
-          ))}
-        </form>
-        {/* Add the submit button */}
-        <button
-          className="btn btn-rounded btn-success"
-          onClick={handleSubmit}
-          disabled={isLoading}
-        >
-          {isLoading ? 'Adding...' : 'Submit'}
-        </button>
+          </div>
+        ) : (
+          <div>
+            <h3>Matching Items:</h3>
+            <form>
+              {matchingItems.map((item, index) => (
+                <div key={index} className="mb-3">
+                  {/* Display matching items */}
+                  {/* ... Include input fields for matching items */}
+                  <button
+                    className="btn mx-1 btn-rounded btn-success"
+                    onClick={() => selectItem(item)}
+                  >
+                    Select
+                  </button>
+                </div>
+              ))}
+            </form>
+          </div>
+        )}
 
-        <button className="btn btn-rounded btn-danger" onClick={closeModal}>Close</button>
+        <button className="btn mx-1 btn-rounded btn-danger" onClick={closeModal}>
+          Close
+        </button>
       </Modal>
     </div>
   );
