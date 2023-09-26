6a21e8d8b12876b838ed26d024103fcde3e1b880
Justin Brierley <91489058+paradingwolves@users.noreply.github.com>
2023-09-26 12:58:07 -0400

    Update AddStock.jsx

--- src/components/AddStock/AddStock.jsx
+++ src/components/AddStock/AddStock.jsx
@@ -6,7 +6,6 @@ import Modal from 'react-modal';
 import useMatchingStockData from '../../hooks/ScanStock'; // Import the hook
 import { db } from '../../lib/firebase'; // Import Firebase configuration
 import { collection, getDocs } from 'firebase/firestore';
-import useAddStock from '../../hooks/AddStock';
 
 
 const AddStock = () => {
@@ -16,13 +15,6 @@ const AddStock = () => {
   const [scanningEnabled, setScanningEnabled] = useState(true); // Control scanner state
   const [stockData, setStockData] = useState([]); // State to hold Stock data
   const { matchingItems, startScanning, stopScanning } = useMatchingStockData(detectedBarcode); // Use the hook
-
-  const {
-    selectedItem,
-    selectItem,
-    clearSelectedItem,
-    updateStockQuantity,
-  } = useAddStock();
 
   const openModal = () => {
     setModalIsOpen(true);
@@ -107,16 +99,12 @@ const AddStock = () => {
 
     fetchStockData();
   }, []);
-  const handleQuantityChange = (event) => {
-    const newQuantity = event.target.value;
-    updateStockQuantity(newQuantity);
-  };
 
   return (
     <div>
       <Header />
       <div className="container bg-light p-4 rounded">
-        <h1 className="text-dark fw-bold text-center">Add Inventory</h1>
+        <h1 className='text-dark fw-bold text-center'>Add Inventory</h1>
         <div className="text-center my-3">
           <h5 className="fw-bold text-dark">Scan Barcodes</h5>
           <video
@@ -135,52 +123,81 @@ const AddStock = () => {
         onRequestClose={closeModal}
         contentLabel="Detected Barcode Modal"
       >
-        {selectedItem ? (
-          <div>
-            <h3>Selected Item:</h3>
-            <div className="mb-3">
-              <label className="form-label">Name</label>
-              <input
-                type="text"
-                className="form-control"
-                value={selectedItem.name}
-                required
-                disabled
-              />
-              {/* ... Include other input fields for the selected item */}
-              <label className="form-label">Quantity</label>
-              <input
-                type="text"
-                className="form-control"
-                required
-                value={selectedItem.quantity}
-                onChange={handleQuantityChange}
-              />
-            </div>
-          </div>
-        ) : (
-          <div>
-            <h3>Matching Items:</h3>
-            <form>
-              {matchingItems.map((item, index) => (
-                <div key={index} className="mb-3">
-                  {/* Display matching items */}
-                  {/* ... Include input fields for matching items */}
-                  <button
-                    className="btn mx-1 btn-rounded btn-success"
-                    onClick={() => selectItem(item)}
-                  >
-                    Select
-                  </button>
-                </div>
-              ))}
-            </form>
-          </div>
-        )}
 
-        <button className="btn mx-1 btn-rounded btn-danger" onClick={closeModal}>
-          Close
-        </button>
+        {/* Display matching items or Stock data */}
+        <h3>Matching Items:</h3>
+        <form>
+    {matchingItems.map((item, index) => (
+      <div key={index} className="mb-3">
+        <label className="form-label">Name</label>
+        <input
+          type="text"
+          className="form-control"
+          value={item.name}
+          required
+          disabled
+        />
+        <label className="form-label">Brand</label>
+        <input
+          type="text"
+          className="form-control"
+          value={item.brand}
+          required
+          disabled
+        />
+        <label className="form-label">Quantity</label>
+        <input
+          type="text"
+          className="form-control"
+          required
+          value={item.quantity}
+        />
+        <label className="form-label">Expiry Date</label>
+        <input
+          type="date"
+          className="form-control"
+          required
+          value={item.expiry_date}
+        />
+        <label className="form-label">Item Number</label>
+        <input
+          type="text"
+          className="form-control"
+          value={item.item_number}
+          required
+          disabled
+        />
+        <label className="form-label">Barcode Number</label>
+        <input
+          type="text"
+          className="form-control"
+          value={item.barcode_number}
+          required
+          disabled
+        />
+        <label className="form-label">Animal</label>
+        <input
+          type="text"
+          className="form-control"
+          value={item.animal}
+          required
+          disabled
+        />
+        <label className="form-label">Updated</label>
+        <input
+          type="text"
+          className="form-control"
+          value="0"
+          required
+          disabled
+        />
+      </div>
+    ))}
+  </form>
+
+
+        <button className="btn mx-1 btn-rounded btn-success" onClick={closeModal}>Submit</button>
+        <button className="btn mx-1 btn-rounded btn-danger" onClick={closeModal}>Close</button>
       </Modal>
     </div>
   );
