cbae331264f6d5568c8e014e6638251ccf4f149f
Justin Brierley <91489058+paradingwolves@users.noreply.github.com>
2023-09-26 12:58:25 -0400

    Update AddStock.js

--- src/hooks/AddStock.js
+++ src/hooks/AddStock.js
@@ -2,29 +2,7 @@
 import { useState } from 'react';
 
 const useAddStock = () => {
-  const [selectedItem, setSelectedItem] = useState(null);
-
-  const selectItem = (item) => {
-    setSelectedItem(item);
-  };
-
-  const clearSelectedItem = () => {
-    setSelectedItem(null);
-  };
-
-  const updateStockQuantity = (newQuantity) => {
-    // Implement the logic to update the stock quantity here
-    // You can use Firebase or any other method to update the quantity
-    // This is just a placeholder function
-    console.log('Updating stock quantity:', newQuantity);
-  };
-
-  return {
-    selectedItem,
-    selectItem,
-    clearSelectedItem,
-    updateStockQuantity,
-  };
+ 
 };
 
 export default useAddStock;
