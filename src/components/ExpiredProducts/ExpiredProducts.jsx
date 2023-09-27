bacfd213d20b0b4c9c4c6168cfcaa93b6e1043d7
Justin Brierley <91489058+paradingwolves@users.noreply.github.com>
2023-09-27 19:24:42 -0400

    Fixed Responsiveness of Expired table

--- src/components/ExpiredProducts/ExpiredProducts.jsx
+++ src/components/ExpiredProducts/ExpiredProducts.jsx
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../Layout/Header';
 import Footer from '../Layout/Footer';
+import { useNavigate } from 'react-router-dom';
 import useExpiredStockData from '../../hooks/ExpiredProducts';
 import useUpdateQuantityToZero from '../../hooks/RemoveStock';
+import { auth } from '../../lib/firebase';
+import useAuth from '../../hooks/Admin';
 import './ExpiredProduct.css';
 
 const ExpiredProducts = () => {
   const { expiredStockData, loading } = useExpiredStockData();
   const { updateQuantityToZero, isLoading: updateLoading } = useUpdateQuantityToZero();
+  const { user } = useAuth();
+  const navigate = useNavigate();
+
+
+  // Use useEffect to log user information after the initial render
+  useEffect(() => {
+    const checkUserAuth = async () => {
+      const unsubscribe = auth.onAuthStateChanged(async (user) => {
+        if (user) {
+          console.log("signed in admin", user.uid);
+        } else {
+          navigate('/login');
+        }
+      });
+
+      return () => unsubscribe();
+    };
+
+    checkUserAuth();
+  }, []); // Empty dependency array to run the effect only once
 
   const handleRemoveProduct = async (name, expiryDate) => {
     // Call the hook to update quantity to zero
@@ -25,36 +48,38 @@ const ExpiredProducts = () => {
           expiredStockData.length === 0 ? (
             <p className='text-center'>No products to display. We aim to keep our inventory fresh. Please remember to sell any items marked with red stickers promptly.</p>
           ) : (
-            <table className="table table-bordered">
-              <thead>
-                <tr>
-                  <th>Name</th>
-                  <th>Brand</th>
-                  <th>Quantity</th>
-                  <th>Expiry Date</th>
-                  <th>Actions</th>
-                </tr>
-              </thead>
-              <tbody>
-                {expiredStockData.map((item) => (
-                  <tr key={item.id}>
-                    <td>{item.name}</td>
-                    <td>{item.brand}</td>
-                    <td>{item.quantity}</td>
-                    <td>{item.expiry_date}</td>
-                    <td>
-                      <button
-                        className="btn btn-danger"
-                        onClick={() => handleRemoveProduct(item.name, item.expiry_date)}
-                        disabled={updateLoading}
-                      >
-                        Remove
-                      </button>
-                    </td>
+            <div className="table-responsive">
+              <table className="table table-bordered">
+                <thead>
+                  <tr>
+                    <th>Name</th>
+                    <th>Brand</th>
+                    <th>Quantity</th>
+                    <th>Expiry Date</th>
+                    <th>Actions</th>
                   </tr>
-                ))}
-              </tbody>
-            </table>
+                </thead>
+                <tbody>
+                  {expiredStockData.map((item) => (
+                    <tr key={item.id}>
+                      <td>{item.name}</td>
+                      <td>{item.brand}</td>
+                      <td>{item.quantity}</td>
+                      <td>{item.expiry_date}</td>
+                      <td>
+                        <button
+                          className="btn btn-danger"
+                          onClick={() => handleRemoveProduct(item.name, item.expiry_date)}
+                          disabled={updateLoading}
+                        >
+                          Remove
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
           )
         )}
       </div>
